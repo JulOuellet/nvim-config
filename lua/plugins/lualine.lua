a02345271bbefe30d3ab40c5b18c4a2330f12c80
Julien ouellet <julienouellet@iusevimbtw.com>
2025-07-14 14:51:42 -0400

    init config with lazy.nvim

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,17 @@
+return {
+    {
+        'nvim-lualine/lualine.nvim',
+        enabled = true,
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = function()
+            require('lualine').setup({
+                options = {
+                    theme = 'auto',
+                    section_separators = '',
+                    component_separators = '',
+                },
+            })
+        end
+    }
+}
+
