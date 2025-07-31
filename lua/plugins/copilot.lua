6a05e908d72160ec15efca8dd5c8c101193c608e
Julien ouellet <julienouellet@iusevimbtw.com>
2025-07-30 22:00:52 -0400

    add copilot plugin

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,23 @@
+return {
+  'zbirenbaum/copilot.lua',
+  cmd = "Copilot",
+  event = "InsertEnter",
+
+  config = function()
+    require("copilot").setup({
+      suggestion = {
+	enabled = true,
+	auto_trigger = true,
+	hide_during_completion = true,
+
+	keymap = {
+	  accept = "<Tab>",
+	  accept_word = "<C-Right>",
+	  next = "<C-Down>",
+	  prev = "<C-Up>",
+	  dismiss = "<C-Left>"
+	}
+      }
+    })
+  end
+}
