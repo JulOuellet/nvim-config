440689354059bd18a759960ecbf3fc38b2948f60
Julien ouellet <julienouellet@iusevimbtw.com>
2025-08-09 22:42:19 -0400

    add formatter

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,29 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        go = { "gofumpt", "goimports" },
+        nix = { "alejandra" },
+        json = { "prettier", "jq" },
+        html = { "prettier" },
+        css = { "prettier" },
+        sql = { "sql-formatter" },
+      },
+      format_on_save = { enabled = false },
+    })
+
+    -- CTRL + ALT + l to format
+    vim.keymap.set(
+      "n",
+      "<C-M-l>",
+      function()
+        require("conform").format({ async = false, lsp_fallback = true })
+      end,
+      { desc = "format file" }
+    )
+  end,
+}
