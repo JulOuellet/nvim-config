015ac2e94ac4f2b821ac38d4cb4e90d5941ecd4d
Julien ouellet <julienouellet@iusevimbtw.com>
2025-09-15 22:29:41 -0400

    better lualine theme

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -7,8 +7,16 @@ return {
 		require("lualine").setup({
 			options = {
 				theme = "auto",
-				section_separators = "",
-				component_separators = "",
+				section_separators = { left = "", right = "" },
+				component_separators = { left = "", right = "" },
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = { "filename" },
+				lualine_x = { "fileformat", "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
 			},
 		})
 	end,
