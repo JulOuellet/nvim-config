e270292b6e0b5d63fd353da223c82b026d67592f
Julien ouellet <julienouellet@iusevimbtw.com>
2025-11-23 00:02:15 -0500

    add which key plugin

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -109,14 +109,14 @@ return {
 			function()
 				Snacks.picker.lsp_symbols()
 			end,
-			desc = "LSP Symbols",
+			desc = "Search LSP Symbols",
 		},
 		{
 			"<leader>sS",
 			function()
 				Snacks.picker.lsp_workspace_symbols()
 			end,
-			desc = "LSP Workspace Symbols",
+			desc = "Search LSP Workspace Symbols",
 		},
 	},
 }
