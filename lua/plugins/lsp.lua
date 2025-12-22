8caa2a2e0dd38d4c517d58ff766c0d61a04272b0
Julien ouellet <julienouellet@iusevimbtw.com>
2025-12-22 15:59:39 -0500

    add inlay hints using lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -27,29 +27,80 @@ return {
 			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 			callback = function(ev)
 				local opts = { buffer = ev.buf }
+				local client = vim.lsp.get_client_by_id(ev.data.client_id)
+
+				if client and client.server_capabilities.inlayHintProvider then
+					vim.lsp.inlay_hint.enable(true, { bufnr = ev.buf })
+				end
 
 				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
 				vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-				vim.keymap.set("n", "<leader>f", function()
-					vim.lsp.buf.format({ async = true })
-				end, opts)
+				vim.keymap.set("n", "<leader>th", function()
+					vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+				end, { buffer = ev.buf, desc = "[T]oggle Inlay [H]ints" })
 			end,
 		})
 
 		local capabilities = require("blink.cmp").get_lsp_capabilities()
 		local lspconfig = require("lspconfig")
 
-		lspconfig.lua_ls.setup({ capabilities = capabilities })
+		lspconfig.lua_ls.setup({
+			capabilities = capabilities,
+			settings = {
+				hint = {
+					enable = true,
+				},
+			},
+		})
+
+		lspconfig.gopls.setup({
+			capabilities = capabilities,
+			settings = {
+				gopls = {
+					hints = {
+						assignVariableTypes = true,
+						compositeLiteralFields = true,
+						compositeLiteralTypes = true,
+						constantValues = true,
+						functionTypeParameters = true,
+						parameterNames = true,
+						rangeVariableTypes = true,
+					},
+				},
+			},
+		})
+
+		lspconfig.ts_ls.setup({
+			capabilities = capabilities,
+			settings = {
+				typescript = {
+					inlayHints = {
+						includeInlayParameterNameHints = "all",
+						includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+						includeInlayFunctionParameterTypeHints = true,
+						includeInlayVariableTypeHints = true,
+						includeInlayPropertyDeclarationTypeHints = true,
+						includeInlayFunctionLikeReturnTypeHints = true,
+						includeInlayEnumMemberValueHints = true,
+					},
+				},
+				javascript = {
+					inlayHints = {
+						includeInlayParameterNameHints = "all",
+						includeInlayVariableTypeHints = true,
+					},
+				},
+			},
+		})
+
+		lspconfig.sqls.setup({ capabilities = capabilities })
+		lspconfig.templ.setup({ capabilities = capabilities })
+		lspconfig.pyright.setup({ capabilities = capabilities })
 		lspconfig.nixd.setup({ capabilities = capabilities })
 		lspconfig.cssls.setup({ capabilities = capabilities })
 		lspconfig.bashls.setup({ capabilities = capabilities })
 		lspconfig.html.setup({ capabilities = capabilities })
 		lspconfig.jsonls.setup({ capabilities = capabilities })
-		lspconfig.gopls.setup({ capabilities = capabilities })
-		lspconfig.sqls.setup({ capabilities = capabilities })
-		lspconfig.templ.setup({ capabilities = capabilities })
-		lspconfig.pyright.setup({ capabilities = capabilities })
-		lspconfig.ts_ls.setup({ capabilities = capabilities })
 	end,
 }
