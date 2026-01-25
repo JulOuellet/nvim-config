4621ec5948d3637139b7fcd31bc9e39365dc536c
Julien ouellet <julienouellet@iusevimbtw.com>
2026-01-25 17:26:25 -0500

    everforest -> catppuccin

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,13 +1,11 @@
 return {
 	"catppuccin/nvim",
-	enabled = true,
 	name = "catppuccin",
 	priority = 1000,
-	lazy = false,
 	config = function()
 		require("catppuccin").setup({
-			flavour = "macchiato",
+			flavour = "frappe", -- latte, frappe, macchiato, mocha
 		})
-		vim.cmd.colorscheme("catppuccin-macchiato")
+		vim.cmd.colorscheme("catppuccin")
 	end,
 }
