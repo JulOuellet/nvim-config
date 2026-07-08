141a5d19c05daed8228e8c34abd8ebcea0fd6029
Julien ouellet <julienouellet@iusevimbtw.com>
2026-07-08 16:56:39 -0400

    update treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,51 +1,39 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	enabled = true,
+	branch = "main",
+	lazy = false,
 	build = ":TSUpdate",
 
 	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"lua",
-				"nix",
-				"css",
-				"html",
-				"json",
-				"go",
-				"templ",
-				"sql",
-				"python",
-				"java",
-				"javascript",
-				"typescript",
-				"bash",
-				"markdown",
-				"markdown_inline",
-				"latex",
-				"typst",
-				"yaml",
-			},
-			auto_install = false,
-			sync_install = false,
-			ignore_install = {},
-			modules = {},
-			highlight = {
-				enable = true,
-			},
-			indent = {
-				enable = true,
-			},
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "gnn", -- Start selection
-					node_incremental = "grn", -- Increment node
-					node_decremental = "grm", -- Decrement node
-				},
-			},
-			autotag = {
-				enable = true,
-			},
+		require("nvim-treesitter").install({
+			"lua",
+			"nix",
+			"css",
+			"html",
+			"json",
+			"go",
+			"templ",
+			"sql",
+			"python",
+			"java",
+			"javascript",
+			"typescript",
+			"bash",
+			"markdown",
+			"markdown_inline",
+			"latex",
+			"typst",
+			"yaml",
+		})
+
+		vim.api.nvim_create_autocmd("FileType", {
+			group = vim.api.nvim_create_augroup("treesitter-start", { clear = true }),
+			callback = function(ev)
+				-- no-op for filetypes without an installed parser
+				if pcall(vim.treesitter.start, ev.buf) then
+					vim.bo[ev.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end
+			end,
 		})
 	end,
 }
