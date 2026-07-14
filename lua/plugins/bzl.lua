346ca33808a66c6139ac09e641c57086941fbcad
Julien ouellet <julienouellet@iusevimbtw.com>
2026-07-14 08:30:24 -0400

    add bzl plugin keymaps

--- lua/plugins/bzl.lua
+++ lua/plugins/bzl.lua
@@ -2,5 +2,50 @@ return {
 	"JulOuellet/bzl.nvim",
 	-- loads ~/projects/bzl.nvim when present (see dev in config/lazy.lua)
 	dev = true,
+	cmd = "Bzl",
+	keys = {
+		{
+			"<leader>bt",
+			function()
+				require("bzl").targets("testable", "here")
+			end,
+			desc = "Bazel Tests (project)",
+		},
+		{
+			"<leader>bT",
+			function()
+				require("bzl").tree("testable")
+			end,
+			desc = "Bazel Tests (workspace)",
+		},
+		{
+			"<leader>br",
+			function()
+				require("bzl").targets("runnable", "here")
+			end,
+			desc = "Bazel Runnables (project)",
+		},
+		{
+			"<leader>bR",
+			function()
+				require("bzl").tree("runnable")
+			end,
+			desc = "Bazel Runnables (workspace)",
+		},
+		{
+			"<leader>bb",
+			function()
+				require("bzl").targets()
+			end,
+			desc = "Bazel All Targets",
+		},
+		{
+			"<leader>bs",
+			function()
+				require("bzl").sync()
+			end,
+			desc = "Bazel Sync",
+		},
+	},
 	opts = {},
 }
